6d96a10b1a5730d61bac5b6052c6433c255402e5
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-15 23:26:07 +0300

    сущность заявок

---
+++ src/main/java/com/example/document_flow/entity/Application.java
@@ -0,0 +1,28 @@
+package com.example.document_flow.entity;
+
+// Сущность заявок
+
+import com.example.document_flow.entity.enums.Status;
+import jakarta.persistence.*;
+import jdk.jfr.Name;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "applications")
+@Data
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //id заявки
+    @Column(name = "userId", nullable = false)
+    private Long applicantId; //id подающего заявку
+    @Column(name = "fullname", nullable = false)
+    private String fullname; //фио подающего заявку
+    private LocalDateTime submissionDateTime; // дата подачи
+    @Enumerated(EnumType.STRING)
+    private Status status; // статус заявки
+    private String documentUrlInMinio; // ссылка на заявку в minIo
+    private String comment; //коммент к заявке (оставляет подающий заявку)
+}
---
+++ src/main/java/com/example/document_flow/entity/enums/Status.java
@@ -0,0 +1,8 @@
+package com.example.document_flow.entity.enums;
+
+public enum Status {
+    PENDING,
+    IN_PROGRESS,
+    REJECTED,
+    READY
+}
