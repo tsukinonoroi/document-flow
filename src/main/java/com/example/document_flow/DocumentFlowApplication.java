e386eb6ac359b4160775018966384187c374d0eb
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-15 23:09:25 +0300

    init project

---
+++ src/main/java/com/example/document_flow/DocumentFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.document_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/document_flow/DocumentFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.document_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
